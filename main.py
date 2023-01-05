cba26ac16c6cd08be2838ec7706a41627fdee259
Rahul Rama Chandran <rahulrchandran00@gmail.com>
2023-01-05 10:28:48 +0530

    Data Validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sensor.utils import get_collection_as_dataframe
 import sys,os
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validatoin import DataValidation
 
 
 
@@ -11,10 +12,19 @@ if __name__ == "__main__":
      try:
           #get_collection_as_dataframe(database_name = 'aps' , collection_name = "sensor")  
           training_pipeline_config = config_entity.TrainingPipelineConfig()
+
+          #data ingestion
           data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
           data_ingestion = DataIngestion(data_ingestion_config = data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+          #data validation
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = DataValidation(data_validation_config = data_validation_config, data_ingestion_artifact = data_ingestion_artifact)
+
+          data_validation_artifact = data_validation.initiate_data_validation()
+
 
      except Exception as e:
           print(e)
