c44618ecfc1bb4bbbb630f3097f44439cb5c0df1
Rahul Rama Chandran <rahulrchandran00@gmail.com>
2023-03-21 17:54:20 +0530

    Model evaluvation first commit

--- sensor/predictor.py
+++ sensor/predictor.py
@@ -30,11 +30,59 @@ class ModelResolver:
 
     def det_latest_model_path(self):
         try:
-            dir_names = os.lisrdir(self.model_registry)
-            if len(dir_names) == 0:
-                return None
-            dir_names = list(map(int, dir_names))
-            latest_dir_name = max(dir_names)
-            return os.path.join(self.model_registry,f"{latest_dir_name}")
+            latest_dir = self.get_latest_dir_path()
+            if latest_dir is None:
+                raise Exception(f"Model is not available")
+            return os.path.join(latest_dir,self.model_dir_name,MODEL_FILE_NAME)
         except Exception as e:
             raise e
+
+    def get_latest_transformer_path(self):
+        try:
+            latest_dir = self.get_latest_dir_path()
+            if latest_dir is None:
+                raise Exception(f"Transformer is not available")
+            return os.path.join(latest_dir,self.transformer_dir_name,TRANSFORMER_OBJECT_FILE_NAME)
+        except Exception as e:
+            raise e
+
+    def get_latest_target_encoder_path(self):
+        try:
+            latest_dir = self.get_latest_dir_path()
+            if latest_dir is None:
+                raise Exception(f"Target encoder is not available")
+            return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
+        except Exception as e:
+            raise e
+
+    def get_latest_save_dir_path(self)->str:
+        try:
+            latest_dir = self.get_latest_dir_path()
+            if latest_dir==None:
+                return os.path.join(self.model_registry,f"{0}")
+            latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
+            return os.path.join(self.model_registry,f"{latest_dir_num+1}")
+        except Exception as e:
+            raise e
+
+    def get_latest_save_model_path(self):
+        try:
+            latest_dir = self.get_latest_save_dir_path()
+            return os.path.join(latest_dir,self.transformer_dir_name, MODEL_FILE_NAME)
+        except Exception as e:
+            raise e
+
+    def get_latest_save_transformer_path(self):
+        try:
+            latest_dir = self.get_latest_save_dir_path()
+            return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORMER_OBJECT_FILE_NAME)
+        except Exception as e:
+            raise e
+
+    def get_latest_save_target_encoder_path(self):
+        try:
+            latest_dir = self.get_latest_save_dir_path()
+            return os.path.join(latest_dir,self.target_encoder_dir_name,TARGET_ENCODER_OBJECT_FILE_NAME)
+        except Exception as e:
+            raise e
+
